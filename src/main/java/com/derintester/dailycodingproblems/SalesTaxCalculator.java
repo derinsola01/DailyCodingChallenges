f2637fc52971252aaa0fb74743fa9b4f2ef5ee31
Derin Gbadebo <daireenonline@yahoo.com>
2019-08-20 07:01:45 -0700

    first commit

---
+++ src/main/java/com/derintester/dailycodingproblems/SalesTaxCalculator.java
@@ -0,0 +1,68 @@
+package com.derintester.dailycodingproblems;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SalesTaxCalculator {
+
+	public static void main(String[] args) {
+		String input1 = "1 book at 12.49";
+		String input2 = "1 music CD at 16.49";
+		String input3 = "1 chocolate bar at 0.85";
+		List<String> orderedGoods = new ArrayList<String>();
+		Map<String, Float> mapToPrint = new HashMap<String, Float>();
+		orderedGoods.add(input1);
+		orderedGoods.add(input2);
+		orderedGoods.add(input3);
+		mapToPrint = tokenizeInput(orderedGoods);
+		printReceipt(mapToPrint);
+	}
+
+	private static void printReceipt(Map<String, Float> mapToPrint) {
+		for (Map.Entry<String,Float> entry : mapToPrint.entrySet())  
+            System.out.println(entry.getKey() + ": " + entry.getValue()); 
+	}
+
+	private static Map<String, Float> tokenizeInput(List<String> inputString) {
+		Map<String, Float> mappedGoods = new HashMap<String, Float>();
+		for(String stringLine: inputString) {
+			String [] arrOfStr = stringLine.split(" at "); 
+			Map<String, Float> tempHolder =  mapStringToLong(arrOfStr);
+			mappedGoods.putAll(tempHolder);
+		}
+		
+//		Float totalSalesTaxHolder = calculateSalesTax(mappedGoods);
+		Float totalAmountHolder =  calculateTotalAmount(mappedGoods);
+		mappedGoods.put("Total", totalAmountHolder);
+		
+		return mappedGoods;
+	}
+
+//	private static Float calculateSalesTax(Map<String, Float> mappedGoods) {
+//		Float totalAmount = 0.00f;
+//		for (Map.Entry<String,Float> entry : mappedGoods.entrySet()) { 
+//			totalAmount += entry.getValue();
+//		}
+//		return totalAmount;
+//	}
+
+	private static Float calculateTotalAmount(Map<String, Float> mappedGoods) {
+		Float totalAmount = 0.00f;
+		for (Map.Entry<String,Float> entry : mappedGoods.entrySet()) { 
+			totalAmount += entry.getValue();
+		}
+		return totalAmount;
+	}
+
+	private static Map<String, Float> mapStringToLong(String[] arrOfStr) {
+		Map<String, Float> returnMap = new HashMap<String, Float>();
+		String arrKey = arrOfStr[0];
+		float arrValue = Float.parseFloat(arrOfStr[1]);
+		returnMap.put(arrKey, arrValue);
+		return returnMap;
+	}
+	
+
+}
