423739b4104909b341bdc46201b1bf12010c91fa
Derin Gbadebo <daireenonline@yahoo.com>
2019-08-21 07:03:18 -0700

    Refactored.

--- src/main/java/com/derintester/dailycodingproblems/DayOneEasy.java
+++ src/main/java/com/derintester/dailycodingproblems/DayOneEasy.java
@@ -3,10 +3,12 @@ package com.derintester.dailycodingproblems;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+
 public class DayOneEasy {
 	
 	private static final Logger logger = LogManager.getLogger(DayOneEasy.class);
 
+	
 	public static void main(String[] args) {
 		int[] givenArray = {10, 15, 3, 7};
 		int summedNumber = 25;
