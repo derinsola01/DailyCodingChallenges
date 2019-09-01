ca7ea9575ad15ee405e26c1deed441b3b81b93f3
Derin Gbadebo <daireenonline@yahoo.com>
2019-08-31 17:17:01 -0700

    Still looking for optimal structure. Tries implementation.

--- src/main/java/com/derintester/dailycodingproblems/august2019/DayElevenMedium.java
+++ src/main/java/com/derintester/dailycodingproblems/august2019/DayElevenMedium.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
