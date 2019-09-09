ee46360ee26d509a40185b037e05b41058971e88
Derin Gbadebo <daireenonline@yahoo.com>
2019-09-08 22:57:01 -0700

    Day Twenty Solution

--- src/main/java/com/derintester/dailycodingproblems/september2019/DayTwentyEasy.java
+++ src/main/java/com/derintester/dailycodingproblems/september2019/DayTwentyEasy.java
@@ -14,6 +14,8 @@ import org.apache.log4j.Logger;
  * 
  * For example, given A = 3 -> 7 -> 8 -> 10 and B = 99 -> 1 -> 8 -> 10, 
  * return the node with value 8.
+ * 
+ * If returned answer is -1, then the linked list did not intersect.
  *
  */
 public class DayTwentyEasy {
@@ -21,20 +23,25 @@ public class DayTwentyEasy {
 	private static final Logger logger = LogManager.getLogger(DayTwentyEasy.class);
 
 	public static void main(String[] args) {
-		logger.debug("Day 20");
-		
-//		{99,1,8,10,11,12,15,2,32,1};
-		int[] arr1 = {99,1,8,10,11,12,15,2,32,1};
-		int[] arr2 = {3,7,8,10,12,13,5};
+//		int[] arr1 = {3,12,7,10,12,13,5};
+//		int[] arr2 = {99,1,8,10,12,12,15,2,32,1};
+		int[] arr1 = {3,7,8,10};
+		int[] arr2 = {99,1,8,10};
 		
 		LinkedList<Integer> list1 = populateLinkedList(arr1);
 		LinkedList<Integer> list2 = populateLinkedList(arr2);
 		
-		int sameNum = 0;
+		int intersectionNum = getListIntersection(list1, list2);
+		logger.debug("intersectionNum is:\t" + intersectionNum);
+		
+	}
+
+	private static int getListIntersection(LinkedList<Integer> list1, LinkedList<Integer> list2) {
+		int intersectionNum = -1;
 		if (list1.size() == list2.size()) {
 			for (int index = 0; index < list1.size(); index++) {
 				if(list1.get(index) == list2.get(index)) {
-					sameNum = list1.get(index);
+					intersectionNum = list1.get(index);
 					break;
 				}
 			}
@@ -42,63 +49,14 @@ public class DayTwentyEasy {
 			LinkedList<Integer> greaterList = (list1.size() > list2.size()) ? list1 : list2;
 			LinkedList<Integer> lesserList = (greaterList.equals(list2)) ? list1 : list2;
 			int diff = greaterList.size() - lesserList.size();
-			logger.debug("greaterList holds:\t" + greaterList);
-			logger.debug("lesserList holds:\t" + lesserList);
-			logger.debug("diff holds:\t" + diff);
+			for(int index = 0; index < lesserList.size(); index++) {
+				if (lesserList.get(index) == greaterList.get(index + diff)) {
+					intersectionNum = lesserList.get(index);
+					break;
+				}
+			}
 		}
-		logger.debug("sameNum holds:\t" + sameNum);
-		
-		
-//		if(list1.size() > list2.size()) {
-//			int diff = Math.abs(list1.size() - list2.size());
-//			for (int index = 0; index < list2.size(); index++) {
-//				if(list1.get(index + diff) == list2.get(index)) {
-//					sameNum = list2.get(index);
-//					logger.debug("if(list1.size() > list2.size()) called");
-//					break;
-//				}
-//			}
-//		} 
-//		else if (list2.size() < list1.size()) {
-//			int diff = Math.abs(list2.size() - list1.size());
-//			for (int index = 0; index < list1.size(); index++) {
-//				if(list1.get(index) == list2.get(index + diff)) {
-//					sameNum = list1.get(index);
-//					logger.debug("else if(list2.size() < list1.size()) called");
-//					break;
-//				}
-//			}
-//		} 
-//		else {
-//			
-//		}
-		
-//		int arr1Length = arr1.length;
-//		int arr2Length = arr2.length;
-//		
-//		for(int index = 0; index < arr1Length; index++) {
-//			list1.add(arr1[index]);
-//		}
-//		
-//		for(int index = 0; index < arr2Length; index++) {
-//			list2.add(arr2[index]);
-//		}
-//		
-//		int sameNum = 0;
-//		if (arr1Length == arr2Length) {
-//			for (int index = 0; index < arr1Length; index++) {
-//				if(list1.get(index) == list2.get(index)) {
-//					sameNum = list1.get(index);
-//					break;
-//				}
-//			}
-//		} else {
-//			int diff = Math.abs(arr1Length - arr2Length);
-//			logger.debug("diff holds:\t" + diff);
-//		}
-		
-		
-		
+		return intersectionNum;
 	}
 	
 	private static LinkedList<Integer> populateLinkedList(int[] givenArray){
