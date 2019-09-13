b9d5c0b1561f3099d2517773a714bc11578edec4
Derin Gbadebo <daireenonline@yahoo.com>
2019-09-12 23:11:28 -0700

    Updated the solution.

--- src/main/java/com/derintester/dailycodingproblems/september2019/CodifySample.java
+++ src/main/java/com/derintester/dailycodingproblems/september2019/CodifySample.java
@@ -1,8 +1,7 @@
 package com.derintester.dailycodingproblems.september2019;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -12,43 +11,28 @@ public class CodifySample {
 	private static final Logger logger = LogManager.getLogger(CodifySample.class);
 	
 	public static void main(String[] args) {
-//		Integer[] arr = {1, 3, 6, 4, 1, 2};
-//		Integer[] arr = {1, 3, 2};
-		Integer[] arr = {-1, -3};
-		List<Integer> arrConverted = Arrays.asList(arr);
-		Arrays.sort(arr);
-		int lowestNum = Collections.min(arrConverted);
-		int highestNum = Collections.max(arrConverted);
-		
-		Integer retNum = getLowestPositiveNum(arrConverted, lowestNum, highestNum);
-		logger.debug("retNum holds:\t" + retNum);
+//		int[] arr = {1, 3, 6, 4, 1, 2};
+//		int[] arr = {1, 3, 2};
+		int[] arr = {-1, -3};
+		int result = solution(arr);
+		logger.debug("retNum holds:\t" + result);
 	}
-
-	private static Integer getLowestPositiveNum(List<Integer> arrConverted, int lowestNum, int highestNum) {
-		Integer numNotInArray = 0;
-		Integer loopingNum = 0;
-		Integer retNum = 0;
-		Integer returnNum = 0;
-		Integer tempLow = (lowestNum <= 0) ? 1 : lowestNum;
-		for (int index = 0; index < arrConverted.size(); index++) {
-			loopingNum = tempLow + index;
-			logger.debug("loopingNum holds:\t" + loopingNum);
-			if (!(String.valueOf(arrConverted).contains(loopingNum.toString()))) {
-				loopingNum = arrConverted.get(index) + 1;
-				numNotInArray = loopingNum;
-			}
+	
+	private static int solution(int[] A) {
+		Set<Integer> integerSet = new HashSet<>();
+		for (int index = 0; index < A.length; index++) {
+		    if (A[index] > 0) {
+		    	integerSet.add(A[index]);
+		    }
 		}
-		if (lowestNum == tempLow) {
-			if(numNotInArray == 0) {
-				returnNum = highestNum + 1 ;
-			} else {
-				returnNum = numNotInArray;
-			}
-		} else {
-			returnNum = tempLow;
+		
+		for (int index2 = 1; index2 <= A.length + 2; index2++) {
+		    if (!integerSet.contains(index2)) {
+		        return index2;
+		    }
 		}
-		retNum = returnNum;
-		return retNum;
+		
+		return 0;
 	}
 
 }
