a3566899859b9b2f18f7fcd85d82a219b69f0d18
Derin Gbadebo <daireenonline@yahoo.com>
2019-09-23 19:41:07 -0700

    Day Thirty Five solution

---
+++ src/main/java/com/derintester/dailycodingproblems/september2019/DayThirtyFive.java
@@ -0,0 +1,29 @@
+package com.derintester.dailycodingproblems.september2019;
+
+import java.util.Arrays;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class DayThirtyFive {
+	
+	private static final Logger logger = LogManager.getLogger(DayThirtyFive.class);
+
+	public static void main(String[] args) {
+		char[] givenCharArray = {'G', 'B', 'R', 'R', 'B', 'R', 'G'};
+		char[] sortedCharArray = sortCharArrayInReverse(givenCharArray);
+		logger.debug("sortedCharArray holds:\t" + Arrays.toString(sortedCharArray));
+	}
+
+	public static char[] sortCharArrayInReverse(char[] givenCharArray) {
+		char[] sortedCharArray = new char[givenCharArray.length];
+		Arrays.sort(givenCharArray);
+		int sortedCharArrayIndex = 0;
+		for(int index = givenCharArray.length - 1; index >= 0; index--) {
+			sortedCharArray[sortedCharArrayIndex] = givenCharArray[index];
+			sortedCharArrayIndex++;
+		}
+		return sortedCharArray;
+	}
+
+}
