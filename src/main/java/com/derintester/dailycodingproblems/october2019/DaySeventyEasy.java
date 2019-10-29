b907b502d02e9ad19bdf4f142a2d4651345a0113
Derin Gbadebo <daireenonline@yahoo.com>
2019-10-28 22:16:23 -0700

    First commit in ages.

---
+++ src/main/java/com/derintester/dailycodingproblems/october2019/DaySeventyEasy.java
@@ -0,0 +1,26 @@
+package com.derintester.dailycodingproblems.october2019;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class DaySeventyEasy {
+	
+	private static final Logger logger = LogManager.getLogger(DaySeventyEasy.class);
+
+	public static void main(String[] args) {
+		int givenNumber = 5;
+		String perfectNumber = getPerfectNumber(givenNumber);
+		logger.debug("perfectNumber holds: " + perfectNumber);
+	}
+
+	private static String getPerfectNumber(int givenNumber) {
+		String returnedString = "";
+		if(givenNumber < 10) {
+			int remainder = 10 - givenNumber;
+			returnedString = givenNumber + "" + remainder;
+//					Integer.toString(givenNumber) + Integer.toString(remainder);
+		}
+		return returnedString;
+	}
+
+}
